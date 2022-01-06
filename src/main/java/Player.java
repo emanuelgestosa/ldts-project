c23f37b22ad5dfea636dd9da4bfa87edc7ec2742
Igor Liberato <igcbsb@gmail.com>
2022-01-06 13:39:12 -0300

    Added Player turn() and additional minor changes

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,18 +1,25 @@
-public class Player extends CardHolder {
-
+public class Player extends CardHolder{
+    private int initialMoney;
+    private String name;
     private int money;
 
     public Player(String name, int money){
-        super(name);
+        super();
+        this.name = name;
         this.money = money;
+        this.initialMoney = money;
     }
 
-    public void play(){
-        // TO DO
+    public void turn(Deck deck, Hand dealerHand){
+        //TODO
     }
 
     public int getMoney(){
         return money;
+    }
+
+    public int getInitialMoney() {
+        return initialMoney;
     }
 
     public void setMoney(int newMoney) {money = newMoney;}
