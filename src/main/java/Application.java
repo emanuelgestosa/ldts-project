930e8e444e82d3a8dc183cd66d9c5eefe33b2eb9
Igor Liberato <igcbsb@gmail.com>
2022-01-06 13:34:06 -0300

    Change Game class to Menu class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        Game game = new Game();
+        Menu game = new Menu();
         game.run();
     }
 }
