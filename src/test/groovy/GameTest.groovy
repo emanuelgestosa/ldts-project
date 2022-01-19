235b4cce0923fa0ce3bb01e61314f83b03eab41f
AngoIa <emanuel-367@hotmail.com>
2022-01-19 22:52:03 +0000

    Fix calculate who won and its tests

--- src/test/groovy/GameTest.groovy
+++ src/test/groovy/GameTest.groovy
@@ -167,155 +167,159 @@ class GameTest extends Specification {
     def "calculateWhoWon test 1 - player won"(){
         given:
         Table table = new Table("Liberato", 100, 2);
-        table.getPlayer().getHand().addCard(new Card("H", "8"))
-        table.getPlayer().getHand().addCard(new Card("S", "A"))
-        table.getDealer().getHand().addCard(new Card("H", "8"))
-        table.getDealer().getHand().addCard(new Card("S", "K"))
+        table.getPlayer().getHand().addCard(new Card("%", "8"))
+        table.getPlayer().getHand().addCard(new Card("*", "A"))
+        table.getDealer().getHand().addCard(new Card("%", "8"))
+        table.getDealer().getHand().addCard(new Card("*", "K"))
         float result;
         when:
-        result = table.calculateWhoWon(table.getPlayer().getHand(), 0)
+        result = table.calculateWhoWon(table.getPlayer().getHand())
         then:
         result == 2;
     }
     def "calculateWhoWon test 2 - dealer won"(){
         given:
         Table table = new Table("Liberato", 100, 2);
-        table.getPlayer().getHand().addCard(new Card("H", "8"))
-        table.getPlayer().getHand().addCard(new Card("S", "A"))
-        table.getDealer().getHand().addCard(new Card("H", "Q"))
-        table.getDealer().getHand().addCard(new Card("S", "K"))
+        table.getPlayer().getHand().addCard(new Card("%", "8"))
+        table.getPlayer().getHand().addCard(new Card("*", "A"))
+        table.getDealer().getHand().addCard(new Card("%", "Q"))
+        table.getDealer().getHand().addCard(new Card("*", "K"))
         float result;
         when:
-        result = table.calculateWhoWon(table.getPlayer().getHand(), 0)
+        result = table.calculateWhoWon(table.getPlayer().getHand())
         then:
         result == 0;
     }
     def "calculateWhoWon test 3 - tie"(){
         given:
         Table table = new Table("Liberato", 100, 2);
-        table.getPlayer().getHand().addCard(new Card("H", "8"))
-        table.getPlayer().getHand().addCard(new Card("S", "A"))
-        table.getDealer().getHand().addCard(new Card("H", "9"))
-        table.getDealer().getHand().addCard(new Card("S", "K"))
+        table.getPlayer().getHand().addCard(new Card("%", "8"))
+        table.getPlayer().getHand().addCard(new Card("*", "A"))
+        table.getDealer().getHand().addCard(new Card("%", "9"))
+        table.getDealer().getHand().addCard(new Card("*", "K"))
         float result;
         when:
-        result = table.calculateWhoWon(table.getPlayer().getHand(), 0)
+        result = table.calculateWhoWon(table.getPlayer().getHand())
         then:
         result == 1;
     }
     def "calculateWhoWon test 4 - player blackjack"(){
         given:
         Table table = new Table("Liberato", 100, 2);
-        table.getPlayer().getHand().addCard(new Card("H", "K"))
-        table.getPlayer().getHand().addCard(new Card("S", "A"))
-        table.getDealer().getHand().addCard(new Card("H", "Q"))
-        table.getDealer().getHand().addCard(new Card("S", "K"))
+        table.getPlayer().getHand().addCard(new Card("%", "K"))
+        table.getPlayer().getHand().addCard(new Card("*", "A"))
+        table.getDealer().getHand().addCard(new Card("%", "Q"))
+        table.getDealer().getHand().addCard(new Card("*", "K"))
         float result;
         when:
-        result = table.calculateWhoWon(table.getPlayer().getHand(), 0)
+        result = table.calculateWhoWon(table.getPlayer().getHand())
         then:
-        result == 2.5;
+        result == 2.5f;
     }
     def "calculateWhoWon test 5 - dealer blackjack"(){
         given:
         Table table = new Table("Liberato", 100, 2);
-        table.getPlayer().getHand().addCard(new Card("H", "9"))
-        table.getPlayer().getHand().addCard(new Card("S", "A"))
-        table.getDealer().getHand().addCard(new Card("H", "A"))
-        table.getDealer().getHand().addCard(new Card("S", "K"))
+        table.getPlayer().getHand().addCard(new Card("%", "9"))
+        table.getPlayer().getHand().addCard(new Card("*", "A"))
+        table.getDealer().getHand().addCard(new Card("%", "A"))
+        table.getDealer().getHand().addCard(new Card("*", "K"))
         float result;
         when:
-        result = table.calculateWhoWon(table.getPlayer().getHand(), 0)
+        result = table.calculateWhoWon(table.getPlayer().getHand())
         then:
-        result == -0.5;
+        result == 0;
     }
     def "calculateWhoWon test 6 - both blackjack"(){
         given:
         Table table = new Table("Liberato", 100, 2);
-        table.getPlayer().getHand().addCard(new Card("H", "Q"))
-        table.getPlayer().getHand().addCard(new Card("S", "A"))
-        table.getDealer().getHand().addCard(new Card("H", "A"))
-        table.getDealer().getHand().addCard(new Card("S", "K"))
+        table.getPlayer().getHand().addCard(new Card("%", "Q"))
+        table.getPlayer().getHand().addCard(new Card("*", "A"))
+        table.getDealer().getHand().addCard(new Card("%", "A"))
+        table.getDealer().getHand().addCard(new Card("*", "K"))
         float result;
         when:
-        result = table.calculateWhoWon(table.getPlayer().getHand(), 0)
+        result = table.calculateWhoWon(table.getPlayer().getHand())
         then:
         result == 1;
     }
     def "calculateWhoWon test 7 - player bust"(){
         given:
         Table table = new Table("Liberato", 100, 2);
-        table.getPlayer().getHand().addCard(new Card("H", "9"))
-        table.getPlayer().getHand().addCard(new Card("S", "K"))
-        table.getPlayer().getHand().addCard(new Card("S", "5"))
-        table.getDealer().getHand().addCard(new Card("H", "A"))
-        table.getDealer().getHand().addCard(new Card("S", "6"))
+        table.getPlayer().getHand().addCard(new Card("%", "9"))
+        table.getPlayer().getHand().addCard(new Card("*", "K"))
+        table.getPlayer().getHand().addCard(new Card("%", "5"))
+        table.getDealer().getHand().addCard(new Card("*", "A"))
+        table.getDealer().getHand().addCard(new Card("%", "6"))
         float result;
         when:
-        result = table.calculateWhoWon(table.getPlayer().getHand(), 0)
+        result = table.calculateWhoWon(table.getPlayer().getHand())
         then:
         result == 0;
     }
     def "calculateWhoWon test 8 - player bust, dealer blackjack"(){
         given:
         Table table = new Table("Liberato", 100, 2);
-        table.getPlayer().getHand().addCard(new Card("H", "9"))
-        table.getPlayer().getHand().addCard(new Card("S", "K"))
-        table.getPlayer().getHand().addCard(new Card("S", "5"))
-        table.getDealer().getHand().addCard(new Card("H", "A"))
-        table.getDealer().getHand().addCard(new Card("S", "K"))
+        table.getPlayer().getHand().addCard(new Card("%", "9"))
+        table.getPlayer().getHand().addCard(new Card("*", "K"))
+        table.getPlayer().getHand().addCard(new Card("*", "5"))
+        table.getDealer().getHand().addCard(new Card("%", "A"))
+        table.getDealer().getHand().addCard(new Card("*", "K"))
         float result;
         when:
-        result = table.calculateWhoWon(table.getPlayer().getHand(), 0)
+        result = table.calculateWhoWon(table.getPlayer().getHand())
         then:
-        result == -0.5;
+        result == 0;
     }
     def "calculateWhoWon test 9 - dealer bust"(){
         given:
         Table table = new Table("Liberato", 100, 2);
-        table.getPlayer().getHand().addCard(new Card("H", "9"))
-        table.getPlayer().getHand().addCard(new Card("S", "K"))
-        table.getDealer().getHand().addCard(new Card("H", "8"))
-        table.getDealer().getHand().addCard(new Card("S", "6"))
-        table.getDealer().getHand().addCard(new Card("S", "K"))
+        table.getPlayer().getHand().addCard(new Card("%", "9"))
+        table.getPlayer().getHand().addCard(new Card("*", "K"))
+        table.getDealer().getHand().addCard(new Card("%", "8"))
+        table.getDealer().getHand().addCard(new Card("*", "6"))
+        table.getDealer().getHand().addCard(new Card("*", "K"))
         float result;
         when:
-        result = table.calculateWhoWon(table.getPlayer().getHand(), 0)
+        result = table.calculateWhoWon(table.getPlayer().getHand())
         then:
         result == 2;
     }
     def "calculateWhoWon test 10 - split"(){
         given:
         Table table = new Table("Liberato", 100, 2);
-        table.getPlayer().getHand().addCard(new Card("H", "9"))
-        table.getPlayer().getHand().addCard(new Card("S", "K"))
-        table.getPlayer().getHand().addCard(new Card("S", "K"))
+        table.getPlayer().getHand().addCard(new Card("%", "9"))
+        table.getPlayer().getHand().addCard(new Card("*", "K"))
+        table.getPlayer().getHand().addCard(new Card("*", "K"))
         table.getPlayer().setSplitHand(new Hand());
-        table.getPlayer().getSplitHand().addCard(new Card("S", "K"))
-        table.getPlayer().getSplitHand().addCard(new Card("S", "9"))
-        table.getDealer().getHand().addCard(new Card("H", "8"))
-        table.getDealer().getHand().addCard(new Card("S", "K"))
+        table.getPlayer().getSplitHand().addCard(new Card("*", "K"))
+        table.getPlayer().getSplitHand().addCard(new Card("*", "9"))
+        table.getDealer().getHand().addCard(new Card("%", "8"))
+        table.getDealer().getHand().addCard(new Card("*", "K"))
         float result;
         when:
-        result = table.calculateWhoWon(table.getPlayer().getHand(), 2)
+        result = table.calculateWhoWon(table.getPlayer().getHand())
+        then:
+        result == 0;
+        when:
+        result = table.calculateWhoWon(table.getPlayer().getSplitHand())
         then:
         result == 2;
     }
     def "calculateWhoWon test 11 - split blackjack"(){
         given:
         Table table = new Table("Liberato", 100, 2);
-        table.getPlayer().getHand().addCard(new Card("H", "9"))
-        table.getPlayer().getHand().addCard(new Card("S", "K"))
-        table.getPlayer().getHand().addCard(new Card("S", "K"))
+        table.getPlayer().getHand().addCard(new Card("%", "9"))
+        table.getPlayer().getHand().addCard(new Card("*", "K"))
+        table.getPlayer().getHand().addCard(new Card("*", "K"))
         table.getPlayer().setSplitHand(new Hand());
-        table.getPlayer().getSplitHand().addCard(new Card("S", "K"))
-        table.getPlayer().getSplitHand().addCard(new Card("S", "A"))
-        table.getDealer().getHand().addCard(new Card("H", "8"))
-        table.getDealer().getHand().addCard(new Card("S", "K"))
+        table.getPlayer().getSplitHand().addCard(new Card("*", "K"))
+        table.getPlayer().getSplitHand().addCard(new Card("*", "A"))
+        table.getDealer().getHand().addCard(new Card("%", "8"))
+        table.getDealer().getHand().addCard(new Card("*", "K"))
         float result;
         when:
-        result = table.calculateWhoWon(table.getPlayer().getHand(), 2)
+        result = table.calculateWhoWon(table.getPlayer().getHand())
         then:
-        result == 2;
+        result == 0;
     }
 }
