e73069496fd368ab90e69686c4ca03f250f14504
AngoIa <emanuel-367@hotmail.com>
2022-01-20 22:57:29 +0000

    Show the cards on the screen

--- src/main/java/blackjack/viewer/game/GameViewer.java
+++ src/main/java/blackjack/viewer/game/GameViewer.java
@@ -2,10 +2,15 @@ package blackjack.viewer.game;
 
 import blackjack.gui.GUI;
 import blackjack.model.Position;
+import blackjack.model.game.cardholder.Card;
+import blackjack.model.game.cardholder.CardHolder;
 import blackjack.model.game.table.Table;
 import blackjack.viewer.Viewer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class GameViewer extends Viewer<Table> {
 
@@ -17,8 +22,41 @@ public class GameViewer extends Viewer<Table> {
     public void drawElements(GUI gui) throws IOException {
         for (int i = 0; i < getModel().getNumberEntries(); i++)
             gui.drawText(
-                    new Position(3 + i*15, gui.getHeight() - 1),
+                    new Position(3 + i * 15, gui.getHeight() - 1),
                     getModel().getEntry(i),
                     getModel().isSelected(i) ? "#FFD700" : "#FFFFFF");
+        drawHands(gui);
+    }
+
+    private void drawHands(GUI gui) throws IOException {
+        drawPlayerHand(gui);
+        drawDealerHand(gui);
+    }
+
+    private void drawPlayerHand(GUI gui) throws IOException {
+        List<Card> cards = new ArrayList<Card>(getModel().getPlayer().getHand().getCards());
+        int drawColumn = (gui.getWidth() - cards.size()) / 2;
+        for (int i = 0; i < cards.size(); i++) {
+            Card card = cards.get(i);
+            gui.drawText(
+                    new Position(drawColumn + i*3, gui.getHeight() - 4),
+                    card.getSymbol() + card.getSuit(),
+                    Objects.equals(card.getSuit(), "&") || Objects.equals(card.getSuit(), "%") ? "#fc1111" : "#FFFFFF"
+            );
+        }
+    }
+
+    private void drawDealerHand(GUI gui) throws IOException {
+        List<Card> cards = new ArrayList<Card>(getModel().getDealer().getHand().getCards());
+        if (cards.size() < 2) cards.add(new Card("?", "?"));
+        int drawColumn = (gui.getWidth() - cards.size()) / 2;
+        for (int i = 0; i < cards.size(); i++) {
+            Card card = cards.get(i);
+            gui.drawText(
+                    new Position(drawColumn + i*3, 2),
+                    card.getSymbol() + card.getSuit(),
+                    Objects.equals(card.getSuit(), "&") || Objects.equals(card.getSuit(), "%") ? "#fc1111" : "#FFFFFF"
+            );
+        }
     }
 }
