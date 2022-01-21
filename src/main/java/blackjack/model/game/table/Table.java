88e3f6e6f94a730705e15ef6a64b33651008cb2a
AngoIa <emanuel-367@hotmail.com>
2022-01-21 21:43:53 +0000

    Revert change to split payout

--- src/main/java/blackjack/model/game/table/Table.java
+++ src/main/java/blackjack/model/game/table/Table.java
@@ -68,10 +68,10 @@ public class Table {
             if (hand.hasBlackjack()) return 1;
             else return 0;
         }
-        if(hand.hasBlackjack()) return 2.5f;
-        if(hand.bust()) return 0;
-        if(dealer.getHand().bust()) return 2;
-        if(hand.getValue() > dealer.getHand().getValue()) return 2;
+        if (hand.hasBlackjack() && !player.isSplit()) return 2.5f;
+        if (hand.bust()) return 0;
+        if (dealer.getHand().bust()) return 2;
+        if (hand.getValue() > dealer.getHand().getValue()) return 2;
         if (hand.getValue() < dealer.getHand().getValue())  return 0;
         return 1;
     }
